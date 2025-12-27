de15246c6f7d7502745ccdcfa4a94a459f733e66
Konrad Jäger <konrad.jaeger@hotmail.com>
2025-12-27 19:52:37 +0100

    neu

--- MonoGameLibrary/obj/Release/net8.0/MonoGameLibrary.AssemblyInfo.cs
+++ MonoGameLibrary/obj/Release/net8.0/MonoGameLibrary.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonoGameLibrary")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c3cacd335607edc149471786a76bec15546fe56")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonoGameLibrary")]
